1bfb3fcc0c8815cf4ddf6f0a3a99714c5ab65305
nguye <giahoangmy@gmail.com>
2019-05-14 18:51:56 +0700

    27/4/2019

--- app/src/main/java/com/appmp3/appnhac/Activity/MainActivity.java
+++ app/src/main/java/com/appmp3/appnhac/Activity/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     ViewPager viewPager;
-    String userName;
+    String userName = "my";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
